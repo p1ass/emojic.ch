0594e289a72f29dc615851735188e63155790841
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-11-07 22:49:14 +0900

    絵文字を合成できた

--- detect_face.py
+++ detect_face.py
@@ -9,16 +9,19 @@ for file in path.iterdir():
 
 path = Path("./inputs/")
 
+angel = cv2.imread("emoji/angel.png",)
+
 for file in path.iterdir():
 
 	cascade_path = "./cv2/data/haarcascade_frontalface_default.xml"
 	# cascade_path = "./cv2/data/haarcascade_frontalface_alt.xml"
 	# cascade_path = "./cv2/data/haarcascade_frontalface_alt2.xml"
 	# cascade_path = "./cv2/data/haarcascade_frontalface_alt_tree.xml"
+	# cascade_path = "./cv2/data/haarcascade_frontalface_alt_tree.xml"
 
 	image_file = str(file.name)
 	image_path = "./inputs/" + image_file
-	output_path = "./outputs/" + image_file
+	output_path = "./outputs/" + str(file.name)
 
 	#ファイル読み込み
 	image = cv2.imread(image_path)
@@ -42,17 +45,18 @@ for file in path.iterdir():
 	#カスケード分類器の特徴量を取得する
 	cascade = cv2.CascadeClassifier(cascade_path)
 
-	facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(min_face_size, min_face_size))
+	faces = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(min_face_size, min_face_size))
 
-	print(facerect)
+	print(faces)
 	color = (255, 255, 255) #白
 
 	# 検出した場合
-	if len(facerect) > 0:
+	if len(faces) > 0:
 
 		#検出した顔を囲む矩形の作成
-		for rect in facerect:
-			cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
+		for face in faces:
+			resized_angel = cv2.resize(angel,(face[2],face[3]))
 
-		#認識結果の保存
-		cv2.imwrite(output_path, image)
+			image[face[1]:face[1]+face[2],face[0]:face[0]+face[3]] = resized_angel
+			#認識結果の保存
+			cv2.imwrite(output_path, image)
