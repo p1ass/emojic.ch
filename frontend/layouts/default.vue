b2e695bcbed5937c03704ef89ac1834824e89437
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-11-13 21:23:07 +0900

    広告の設定を変更

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,15 +3,18 @@
     <header-nav/>
 
     <nuxt/>
+    <footer-nav/>
   </div>
 </template>
 
 <script>
 import HeaderNav from '~/components/HeaderNav'
+import FooterNav from '~/components/FooterNav'
 
 export default {
   components: {
-    HeaderNav
+    HeaderNav,
+    FooterNav
   }
 }
 </script>
