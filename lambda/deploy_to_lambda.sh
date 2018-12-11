47293c2aa38c67470f9fbc29bb8bf10758ac1a3f
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-12-11 11:59:47 +0900

    デプロイシェルを修正

--- lambda/deploy_to_lambda.sh
+++ lambda/deploy_to_lambda.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm *.zip
-zip -r upload.zip *
+zip -r upload_dev.zip * -x *.z
 aws s3 cp ./upload.zip s3://$EMOJIC_LAMBDA_BUCKET
 aws lambda update-function-code --function-name $EMOJIC_LAMBDA_NAME --s3-bucket $EMOJIC_LAMBDA_BUCKET --s3-key upload.zip
